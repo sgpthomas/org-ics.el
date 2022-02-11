3678b457423eeec6be6eea452578963f2af222f1
Sammy Thomas <sgpthomas@gmail.com>
2022-02-10 22:29:39 -0600

    fixed buffer insertion

--- org-ics.el
+++ org-ics.el
@@ -13,7 +13,9 @@
 
 (defun org-ics/split-lines (lines acc build)
   (if (null lines)
-      acc
+      (progn
+	(message "%s" acc)
+	acc)
     (let* ((line (car lines))
 	   (rest (cdr lines)))
       ;; (message "'%s'" line)
@@ -22,8 +24,8 @@
 	    ;; (message "yes")
 	    (org-ics/split-lines rest
 				 acc
-				 (cons (s-chomp (s-trim line)) build)))
-	(let* ((line (s-trim line))
+				 (cons (s-chop-prefix " " line) build)))
+	(let* (;; (line (s-chomp line))
 	       (build-acc (s-join "" (cons line build)))
 	       (acc (if (null build)
 			(cons line acc)
@@ -33,13 +35,10 @@
 (defun org-ics/prepare-input (text)
   (org-ics/split-lines (reverse (s-split "\n" text)) '() '()))
 
-;; (with-current-buffer "test.org"
-;;   (erase-buffer)
-;;   (insert (s-join "\n"
-;; 		  (org-ics/prepare-input (f-read-text "~/OrgFiles/org-data/test.ics")))))
-
 (defun org-ics/split (string)
   (let ((parts (s-split-up-to ":" string 1)))
+    (when (s-equals? (car parts) "DESCRIPTION")
+      (message "%s" (cadr parts)))
     `(,(car parts) . ,(cadr parts))))
 
 (defun org-ics/parse (lines acc)
@@ -55,28 +54,27 @@
 	    ((s-equals? key "END")
 	     (cons (cdr lines) (reverse acc)))
 	    (t
-	     (let ((pair `(,key . ,(format "%s" val))))
+	     (let ((pair `(,key . ,val)))
 	       (org-ics/parse (cdr lines) (cons pair acc))))))))
+
+(defun org-ics/unescape (string)
+  (let* ((a (replace-regexp-in-string "\\\\n" "\n" string))
+	 (b (replace-regexp-in-string "\\\\\\(.\\)" "\\1" a)))
+    b))
 
 (defun org-ics/to-org-event (event)
   (let* ((summary (cdr (assoc "SUMMARY" event)))
 	 (uid (cdr (assoc "UID" event)))
 	 (dtstart (cdr (--find (s-starts-with? "DTSTART" (car it)) event)))
 	 (dtend (cdr (--find (s-starts-with? "DTEND" (car it)) event)))
-	 (descr (assoc "DESCRIPTION" event))
-	 (location (cdr (assoc "LOCATION" event)))
-	 )
+	 (descr (cdr (assoc "DESCRIPTION" event)))
+	 (location (cdr (assoc "LOCATION" event))))
     (s-join "\n"
-	    (-concat
-	     (list (format "* %s" summary)
-		   (format ":PROPERTIES:")
-		   (format ":END:")
-		   ""
-		   ;; (format "%s" descr)
-		   )
-	     (list (format "%s" (s-split "\n" (cdr descr))))
-	     ;; 
-	     ))))
+	    (list (format "* %s" summary)
+		  ":PROPERTIES:"
+		  ":END:"
+		  ""
+		  (org-ics/unescape descr)))))
 
 (defun org-ics/process (text)
   (let* ((input (org-ics/prepare-input text))
@@ -85,9 +83,12 @@
 	 (events (--filter (s-equals? (car it) "VEVENT") cal))
 	 (org-events (--map (org-ics/to-org-event (cdr it)) events))
 	 (res org-events))
-    (s-join "\n" res)
-    )
-  )
+    (s-concat
+     "#+TITLE: Calendar\n"
+     "#+CATEGORY: Calendar\n"
+     "#+FILETAGS: EVENT\n"
+     "\n"
+     (s-join "\n" res))))
 
 (defun org-ics/import-ics-url-to-org (ics-url org-file-name)
   "Download .ics file form `ics-url' and save to `org-file-name'."
@@ -97,16 +98,14 @@
       :complete (cl-function (lambda (&key data &allow-other-keys) (setq msg data))))
     (with-current-buffer (get-buffer-create org-file-name)
       (erase-buffer)
-      (insert (org-ics/process msg))
-      (pp-buffer))))
+      (insert (org-ics/process msg)))))
 
 (defun org-ics/import-ics-file-to-org (ics-file org-file-name)
   "Download .ics file form `ics-url' and save to `org-file-name'."
   (let ((text (f-read-text ics-file)))
     (with-current-buffer (get-buffer-create org-file-name)
       (erase-buffer)
-      (insert (org-ics/process text))
-      (pp-buffer))))
+      (insert (org-ics/process text)))))
 
 ;; (org-ics/import-ics-url-to-org
 ;;  "https://calendar.google.com/calendar/ical/sgtpeacock%40utexas.edu/private-6382215cc9d4e1bb8659bbe82e5f7a0a/basic.ics"
